5c7b865a28e4ad0fcca34e10be0dfc9a19fea40e
karshh <karsh.sharma@uwaterloo.ca>
2018-04-17 06:43:20 -0600

    begun contact form validation.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -8,16 +9,48 @@ import { Component, OnInit } from '@angular/core';
 export class ContactComponent implements OnInit {
 
 	link: string;
+  contactForm: FormGroup;
+
+  name: FormControl;
+  email: FormControl;
+  subject: FormControl;
+  message: FormControl;
+
+  errMessage: string;
 
   constructor() { 
+    this.errMessage = '';
   	this.link = 'contact';
+    this.createFormControls();
+    this.contactForm = new FormGroup({
+      name: this.name,
+      email: this.email,
+      subject: this.subject,
+      message: this.message
+    });
+     
+  }
+
+  private createFormControls(): void { 
+    this.name = new FormControl('', Validators.required);
+    this.email = new FormControl('', [ Validators.required, Validators.email]);
+    this.subject = new FormControl('', Validators.required);
+    this.message = new FormControl('', [Validators.required]);
   }
 
   ngOnInit() {
   }
 
-  onSubmit(form: any): void {
-  	console.log(form);
+  onSubmit(): void {
+    if (!this.contactForm.valid) {
+      this.errMessage = 'Please fix the invalid inputs.';
+      return;
+    }
+
+
+    this.errMessage = '';
+    console.log('Form submitted:', this.contactForm.value);
+    this.contactForm.reset();
   }
 
 }
